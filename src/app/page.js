a0b78d53b676274f32b7d9d4f3ead0f2dd8454a7
ERICK5940 <brotherssk81@gmail.com>
2025-07-21 19:56:20 +0530

    21.07.2025(add some styles and animations)

--- src/app/page.js
+++ src/app/page.js
@@ -7,32 +7,71 @@ export default function LoginPage() {
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
+
     const student = new Student(name, password);
     const message = student.login();
     alert(message);
   };
 
   return (
-    <center>
-    <div>
-      <h2>Student Login</h2>
-      <form onSubmit={handleLogin}>
+    <div style={styles.container}>
+      <h2>🎓 Student Login</h2>
+      <form onSubmit={handleSubmit} style={styles.form}>
         <input
           type="text"
-          placeholder="Name"
+          placeholder="Enter name"
           value={name}
           onChange={(e) => setName(e.target.value)}
-        /><br /><br />
+          style={styles.input}
+        />
         <input
           type="password"
-          placeholder="Password"
+          placeholder="Enter password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-        /><br /><br />
-        <button type="submit">Login</button>
+          style={styles.input}
+        />
+        <button type="submit" style={styles.button}>Login</button>
       </form>
-    </div></center>
+    </div>
   );
 }
+
+const styles = {
+  container: {
+    maxWidth: "400px",
+    margin: "100px auto",
+    padding: "20px",
+    background: "linear-gradient(270deg ,yellow,orange,red)",
+    boxShadow: "0 0 10px rgba(0,0,0,0.1)",
+    borderRadius: "10px",
+    justifycontent:"center",
+    Alignitems: "center",
+  },
+  form: {
+    display: "flex",
+    alignitems:"center",
+    flexDirection: "column",
+    padding:"20px",
+    gap: "13px",
+  },
+  input: {
+    padding: "10px",
+    fontSize: "16px",
+    borderRadius:"5px"
+  },
+  button: {
+    maxWidth:"350gpx",
+    padding: "10px 20px",
+    background: "linear-gradient(to right ,blue,red,purple)",
+    color: "white",
+    border: "none",
+    cursor:"pointer",
+    transition:"0.3",
+    borderRadius: "5px",
+    fontWeight: "bold",
+    textAlign:"center",
+  },
+};
